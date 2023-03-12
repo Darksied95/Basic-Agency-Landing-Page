a3cc7f42d0213fb4e75f7fdff8000a8b863fa077
Darksied95 <aliyuefe9501@gmail.com>
2023-03-12 22:56:47 +0100

    initial commit

---
+++ src/Component/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Logo from "../Assets/logo.svg";
+import Close from "../Assets/close.svg";
+import Sign from "../Assets/sign.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="Basic/Dept" />
+      <div>
+        <div>
+          <img src={Logo} alt="Basic/Dept" />
+          <img src={Close} alt="Basic/Dept" />
+        </div>
+        <ul>
+          <li>
+            <a href="/">Work</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">news</a>
+          </li>
+          <li>
+            <a href="/">Thinking</a>
+          </li>
+          <li>
+            <a href="/">Careers</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <a href="/">
+              initiatives <img src={Sign} alt="" />
+            </a>
+          </li>
+        </ul>
+        <div>
+          <span>BASIC/DEPT®, inc</span>
+          <span>10 - 23©</span>
+        </div>
+      </div>
+      <button>Menu</button>
+    </header>
+  );
+};
+
+export default Header;
