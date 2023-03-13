dfc3b3b17d6261f52d4b2b3ce6e5b8bfeaca5c73
Darksied95 <aliyuefe9501@gmail.com>
2023-03-13 18:28:18 +0100

    added mouseleaveHandler

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -8,18 +8,29 @@ import "./Header.css";
 
 const Header = () => {
   const cursorRef = useRef();
+
   const mousemoveHandler = (e) => {
     const { clientX, clientY } = e;
     cursorRef.current.style.top = clientY - 35 + "px";
     cursorRef.current.style.left = clientX - 35 + "px";
+  };
+  const mouseleaveHandler = (e) => {
+    cursorRef.current.style.top = "50%";
+    cursorRef.current.style.left = "50%";
   };
   useEffect(() => {
     document
       .querySelector("#header")
       .addEventListener("mousemove", mousemoveHandler);
 
+    document
+      .querySelector("#header")
+      .addEventListener("mouseleave", mouseleaveHandler);
+
     return () => {
-      document.removeEventListener("mousemove", mousemoveHandler);
+      document
+        .querySelector("#header")
+        .removeEventListener("mousemove", mousemoveHandler);
     };
   }, []);
 
@@ -30,17 +41,18 @@ const Header = () => {
     >
       <div
         ref={cursorRef}
-        className="absolute top-1/2 left-1/2  w-24 flex flex-col justify-center "
+        className="absolute top-1/2 left-1/2  w-24 flex flex-col justify-center lg:w-32 "
       >
         <div className="bg-white rounded-[50%]  aspect-square text-center uppercase mx-3 mb-2 grid place-content-center">
-          <p className="text-xs">
+          <p className="text-[11px] font-semibold leading-3 lg:text-[15px] lg:leading-5">
             Play <br />
             Reel
           </p>
         </div>
 
-        <div className="text-white text-[10px] text-center">
+        <div className="text-white text-[10px] text-center font-bold lg:text-[13px]">
           <span>BASIC/DEPT®</span>
+
           <br />
           <span>2010 - 23©</span>
         </div>
