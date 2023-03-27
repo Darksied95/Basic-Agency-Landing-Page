fe6f5935426614079236cc9703defcc1cfc460e0
Darksied95 <aliyuefe9501@gmail.com>
2023-03-27 21:56:26 +0100

    extracted header hover

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,46 +2,19 @@ import React from "react";
 import Logo from "../../Assets/Logo.jsx";
 import Video from "../../Assets/Header-video.mp4";
 import More from "../../Assets/more.svg";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Header-Modal";
 import "./Header.css";
+import HeaderHover from "./HeaderHover.jsx";
 
 const Header = () => {
-  const [showCursor, setshowCursor] = useState(false);
-  const [showModal, setshowModal] = useState(false);
-  const cursorRef = useRef();
+  const [showCursor, setShowCursor] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  const mousemoveHandler = (e) => {
-    const { clientX, clientY } = e;
-    if (clientY < 90) {
-      mouseleaveHandler();
-      setshowCursor(true);
-      return;
-    }
-    cursorRef.current.style.top = clientY - 35 + "px";
-    cursorRef.current.style.left = clientX - 35 + "px";
-    setshowCursor(false);
-  };
-  const mouseleaveHandler = () => {
-    cursorRef.current.style.top = "50%";
-    cursorRef.current.style.left = "50%";
-  };
-  useEffect(() => {
-    document
-      .querySelector("#header")
-      .addEventListener("mousemove", mousemoveHandler);
-
-    document
-      .querySelector("#header")
-      .addEventListener("mouseleave", mouseleaveHandler);
-
-    return () => {
-      document
-        .querySelector("#header")
-        .removeEventListener("mousemove", mousemoveHandler);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const handleShowCursor = () => setShowCursor(true);
+  const handleHideCursor = () => setShowCursor(false);
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
     <header
@@ -51,24 +24,11 @@ const Header = () => {
       }`}
     >
       {showModal && <Modal onCloseModal={handleCloseModal} />}
-      <div
-        ref={cursorRef}
-        className="absolute top-1/2 left-1/2  w-24 flex flex-col justify-center lg:w-32 select-none"
-      >
-        <div className="bg-white rounded-[50%]  aspect-square text-center uppercase mx-3 mb-2 grid place-content-center">
-          <p className="text-[11px] font-semibold leading-3 lg:text-[15px] lg:leading-5">
-            Play <br />
-            Reel
-          </p>
-        </div>
 
-        <div className="text-white text-[10px] text-center font-bold lg:text-[13px]">
-          <span>BASIC/DEPT®</span>
-
-          <br />
-          <span>2010 - 23©</span>
-        </div>
-      </div>
+      <HeaderHover
+        handleShowCursor={handleShowCursor}
+        handleHideCursor={handleHideCursor}
+      />
 
       <video
         // autoPlay
@@ -107,7 +67,7 @@ const Header = () => {
       </nav>
       <button
         className="text-2xl text-[#f4f4f4] self-start xl:hidden"
-        onClick={() => setshowModal(true)}
+        onClick={handleOpenModal}
       >
         Menu
       </button>
@@ -116,9 +76,6 @@ const Header = () => {
       </button>
     </header>
   );
-  function handleCloseModal() {
-    setshowModal(false);
-  }
 };
 
 export default Header;
---
+++ src/Component/Header/HeaderHover.jsx
@@ -0,0 +1,59 @@
+import { useRef, useEffect } from "react";
+
+const HeaderHover = ({ handleShowCursor, handleHideCursor }) => {
+  const cursorRef = useRef();
+
+  const mousemoveHandler = (e) => {
+    const { clientX, clientY } = e;
+    if (clientY < 90) {
+      mouseleaveHandler();
+      handleShowCursor();
+      return;
+    }
+    cursorRef.current.style.top = clientY - 35 + "px";
+    cursorRef.current.style.left = clientX - 35 + "px";
+    handleHideCursor();
+  };
+  const mouseleaveHandler = () => {
+    cursorRef.current.style.top = "50%";
+    cursorRef.current.style.left = "50%";
+  };
+  useEffect(() => {
+    document
+      .querySelector("#header")
+      .addEventListener("mousemove", mousemoveHandler);
+
+    document
+      .querySelector("#header")
+      .addEventListener("mouseleave", mouseleaveHandler);
+
+    return () => {
+      document
+        .querySelector("#header")
+        .removeEventListener("mousemove", mousemoveHandler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div
+      ref={cursorRef}
+      className="absolute top-1/2 left-1/2  w-24 flex flex-col justify-center lg:w-32 select-none"
+    >
+      <div className="bg-white rounded-[50%]  aspect-square text-center uppercase mx-3 mb-2 grid place-content-center">
+        <p className="text-[11px] font-semibold leading-3 lg:text-[15px] lg:leading-5">
+          Play <br />
+          Reel
+        </p>
+      </div>
+
+      <div className="text-white text-[10px] text-center font-bold lg:text-[13px]">
+        <span>BASIC/DEPT®</span>
+
+        <br />
+        <span>2010 - 23©</span>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderHover;
