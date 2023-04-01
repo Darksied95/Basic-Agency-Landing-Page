d5be3d4ef0c9304ac6e43f320a01062b4e530b60
Darksied95 <aliyuefe9501@gmail.com>
2023-04-01 13:07:30 +0100

    fixed media slider padding issue

--- src/Component/Main/SectionOne/MediaSlider.jsx
+++ src/Component/Main/SectionOne/MediaSlider.jsx
@@ -5,22 +5,22 @@ import GoogleStore from "../../../Assets/GoogleStore.mp4";
 
 const MediaSlider = () => {
   return (
-    <div className="flex mt-8 overflow-x-auto mb-20 scrollbar-none">
-      <div className="min-w-[70vw]  mr-5 inline-block">
+    <div className="flex mt-8 overflow-x-auto mb-20 relative z-50 pl-5 xs:pl-9 md:pl-14">
+      <div className="min-w-[70vw] md:min-w-[50vw]  mr-5">
         <img src={RockClimbing} alt="/" />
         <h1 className="font-bold my-2 text-lg">PATAGONIA </h1>
         <p className="max-w-[110px] uppercase font-bold text-xs opacity-70 ">
           An Ecommerce Experience driven by patagonia's brand mission
         </p>
       </div>
-      <div className="min-w-[70vw] mr-5 inline-block">
+      <div className="min-w-[70vw] md:min-w-[50vw] mr-5">
         <img src={Tennis} alt="" />
         <h1 className="font-bold my-2 text-lg"> WILSON</h1>
         <p className="max-w-[110px] uppercase font-bold text-xs opacity-70 ">
           A century-old sports brand finding its place in culture
         </p>
       </div>
-      <div className="min-w-[70vw] mr-5 inline-block">
+      <div className="min-w-[70vw] md:min-w-[50vw]">
         <video autoPlay loop muted>
           <source src={GoogleStore} type="video/mp4" />
         </video>
--- src/Component/Main/SectionOne/SectionOne.jsx
+++ src/Component/Main/SectionOne/SectionOne.jsx
@@ -4,22 +4,24 @@ import MediaSlider from "./MediaSlider";
 
 const SectionOne = () => {
   return (
-    <section className="px-5 xs:px-9 md:px-14">
-      <div>
-        <p className="font-semibold text-xl leading-6 mb-7 xs:max-w-[80vw] md:max-w-[48.5vw]">
+    <section className="">
+      <div className="xl:flex flex-wrap px-5 xs:px-9 md:px-12">
+        <p className="font-semibold text-xl leading-6 mb-7 xs:max-w-[80vw] md:max-w-[48.5vw] order-1 xl:text-4xl">
           BASIC/DEPT® is a global branding and digital design agency building
           products, services, and eCommerce experiences that turn cultural
           values into company value.
         </p>
-        <button className="border border-black border-solid mb-16 px-8 py-2 rounded-full font-bold text-xs">
+        <button className="border border-black border-solid mb-16 px-8 py-2 rounded-full font-bold text-xs order-3">
           <a href="/">SEE THE WORK</a>
         </button>
+        <figure className="w-[53vw] ml-auto order-2 xl:w-[36.4vw] xl:h-[11vh]">
+          <img src={BandD} alt="" />
+        </figure>
       </div>
-      <figure className="w-[53vw] ml-auto">
-        <img src={BandD} alt="" />
-      </figure>
+
       <MediaSlider />
-      <div className="flex items-center justify-between border-t border-solid  border-black pt-4">
+
+      <div className="flex items-center justify-between border-t border-solid  border-black pt-4 mx-5 xs:mx-9 md:mx-12">
         <p className=" text-xs xl:flex-auto xl:flex xl:justify-between xl:text-sm">
           <span>00</span>
           <br className="xl:hidden" />
