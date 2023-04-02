5a21162a0b71eccfb1404eb7241c6e96e952985b
Darksied95 <aliyuefe9501@gmail.com>
2023-04-02 11:16:47 +0100

    fixed media slider height bug

--- src/Component/Main/SectionOne/MediaSlider.jsx
+++ src/Component/Main/SectionOne/MediaSlider.jsx
@@ -5,22 +5,22 @@ import GoogleStore from "../../../Assets/GoogleStore.mp4";
 
 const MediaSlider = () => {
   return (
-    <div className="flex mt-8 overflow-x-auto mb-20 scrollbar-none z-50 pl-5 xs:pl-9 md:pl-14 xl:px-14">
-      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-[20vw]  mr-5">
-        <img src={RockClimbing} alt="/" />
+    <div className="flex mt-8 overflow-x-auto mb-20 scrollbar-none z-50 pl-5 xs:px-9 md:px-14">
+      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-0  mr-5 xl:flex-1">
+        <img src={RockClimbing} alt="/" className="" />
         <h1 className="font-bold my-2 text-lg">PATAGONIA </h1>
         <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
           An Ecommerce Experience driven by patagonia's brand mission
         </p>
       </div>
-      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-[20vw] mr-5">
+      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-0 mr-5 xl:flex-1">
         <img src={Tennis} alt="" />
         <h1 className="font-bold my-2 text-lg"> WILSON</h1>
         <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
           A century-old sports brand finding its place in culture
         </p>
       </div>
-      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-[20vw] ">
+      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-0 xl:flex-1">
         <video autoPlay loop muted>
           <source src={GoogleStore} type="video/mp4" />
         </video>
--- src/Component/Main/SectionOne/SectionOne.jsx
+++ src/Component/Main/SectionOne/SectionOne.jsx
@@ -5,7 +5,7 @@ import MediaSlider from "./MediaSlider";
 const SectionOne = () => {
   return (
     <section className="">
-      <div className="xl:flex flex-wrap px-5 xs:px-9 md:px-12">
+      <div className="xl:flex flex-wrap px-5 xs:px-9 md:px-12 xl:gap-6">
         <p className="font-semibold text-xl leading-6 mb-7 xs:max-w-[80vw] md:max-w-[48.5vw] order-1 xl:text-4xl">
           BASIC/DEPT® is a global branding and digital design agency building
           products, services, and eCommerce experiences that turn cultural
@@ -14,9 +14,9 @@ const SectionOne = () => {
         <button className="border border-black border-solid mb-16 px-8 py-2 rounded-full font-bold text-xs order-3">
           <a href="/">SEE THE WORK</a>
         </button>
-        <figure className="w-[53vw] ml-auto order-2 xl:w-[36.4vw] xl:h-[11vh]">
+        <div className="w-[53vw] ml-auto order-2  xl:w-[36.4vw] xl:h-[11vh]">
           <img src={BandD} alt="" />
-        </figure>
+        </div>
       </div>
 
       <MediaSlider />
