3488b04dee063de397b56fc3b1ca756b3ec4968b
Darksied95 <aliyuefe9501@gmail.com>
2023-04-02 11:53:27 +0100

    made media slider semantically correct

--- src/Component/Main/SectionOne/MediaSlider.jsx
+++ src/Component/Main/SectionOne/MediaSlider.jsx
@@ -5,38 +5,42 @@ import GoogleStore from "../../../Assets/GoogleStore.mp4";
 
 const MediaSlider = () => {
   return (
-    <div className="flex mt-8 overflow-x-auto mb-20 scrollbar-none z-50 pl-5 xs:px-9 md:px-14">
-      <div className="image-container min-w-[70vw] md:min-w-[40vw] xl:min-w-0  mr-5 xl:flex-1  overflow-hidden">
-        <div className="overflow-hidden">
-          <img src={RockClimbing} alt="/" />
-        </div>
-
-        <h1 className="font-bold my-2 text-lg">PATAGONIA </h1>
-        <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
-          An Ecommerce Experience driven by patagonia's brand mission
-        </p>
-      </div>
-      <div className="image-container min-w-[70vw] md:min-w-[40vw] xl:min-w-0 mr-5 xl:flex-1">
-        <div className="overflow-hidden">
-          <img src={Tennis} alt="" />
-        </div>
-        <h1 className="font-bold my-2 text-lg"> WILSON</h1>
-        <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
-          A century-old sports brand finding its place in culture
-        </p>
-      </div>
-      <div className="min-w-[70vw] md:min-w-[40vw] xl:min-w-0 xl:flex-1">
-        <video autoPlay loop muted>
-          <source src={GoogleStore} type="video/mp4" />
-        </video>
-        <h1 className="font-bold my-2 text-lg">GOOGLE STORE </h1>
-        <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
-          {" "}
-          An ecommerce experience helping google bring its hardware to people
-          across the globe
-        </p>
-      </div>
-    </div>
+    <ul className="flex mt-8 overflow-x-auto mb-44 scrollbar-none z-50 pl-5 xs:px-9 md:px-14">
+      <li className="media-container min-w-[70vw] md:min-w-[40vw] xl:min-w-0  mr-5 xl:flex-1  overflow-hidden">
+        <a href="/">
+          <div className="overflow-hidden">
+            <img src={RockClimbing} alt="/" />
+          </div>
+          <h1 className="font-bold my-2 text-lg">PATAGONIA </h1>
+          <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
+            An Ecommerce Experience driven by patagonia's brand mission
+          </p>
+        </a>
+      </li>
+      <li className="media-container min-w-[70vw] md:min-w-[40vw] xl:min-w-0 mr-5 xl:flex-1">
+        <a href="/">
+          <div className="overflow-hidden">
+            <img src={Tennis} alt="" />
+          </div>
+          <h1 className="font-bold my-2 text-lg"> WILSON</h1>
+          <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
+            A century-old sports brand finding its place in culture
+          </p>
+        </a>
+      </li>
+      <li className="media-container min-w-[70vw] md:min-w-[40vw] xl:min-w-0 xl:flex-1">
+        <a href="/">
+          <video autoPlay loop muted>
+            <source src={GoogleStore} type="video/mp4" />
+          </video>
+          <h1 className="font-bold my-2 text-lg">GOOGLE STORE </h1>
+          <p className="max-w-[39.5%] md:max-w-[70%] uppercase font-bold text-xs opacity-70 ">
+            An ecommerce experience helping google bring its hardware to people
+            across the globe
+          </p>
+        </a>
+      </li>
+    </ul>
   );
 };
 
