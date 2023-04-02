2796f65dc357e85330350a2f15a3650876a33190
Darksied95 <aliyuefe9501@gmail.com>
2023-04-02 12:39:30 +0100

    added sectionTwo boilerplate code

--- src/Component/Main/SectionTwo/SectionTwo.jsx
+++ src/Component/Main/SectionTwo/SectionTwo.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import data from "./data";
 
 const SectionTwo = () => {
-  return <section>SectionTwo</section>;
+  return (
+    <section>
+      <h1>Featured Engagement</h1>
+      <ul>
+        {data.map(({ link, Icon, Name, Text }, index) => (
+          <li key={index}>
+            <div>
+              <img src={Icon} alt="/" />
+              <span className="block w-5 h-1 bg-black"></span>
+            </div>
+            <h2>{Name}</h2>
+            <p>
+              {Text}
+              {link && <a href="/">here.</a>}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default SectionTwo;
