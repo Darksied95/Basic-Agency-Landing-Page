72402e7e6c63a8cf3987992bf1d5cfccb41140d7
Darksied95 <aliyuefe9501@gmail.com>
2023-04-06 01:19:35 +0100

    picked useRef over useState cause of slow slider rendering

--- src/Component/Main/SectionTwo/SectionTwo.jsx
+++ src/Component/Main/SectionTwo/SectionTwo.jsx
@@ -5,21 +5,20 @@ const SectionTwo = () => {
   const sliderCounter = useRef();
 
   function updateSlider(num) {
-    sliderCounter.current.style.left = num + "%";
+    let stringinfyNum = num + "%";
+    sliderCounter.current.style.left = stringinfyNum;
+    sliderCounter.current.style.transform = `translateX(-${stringinfyNum})`;
   }
 
   return (
     <section>
       <h1>Featured Engagement</h1>
       <ArticleSlider updateSlider={updateSlider} />
-      <div className=" bottom-0 h-[1px]  bg-gray-500 w-[90vw] m-auto right-[2rem] ">
+      <div className=" h-[1px]  bg-gray-500 w-[90vw] m-auto ">
         <div className="relative w-full h-full">
           <span
             ref={sliderCounter}
             className="absolute block h-full w-[30%] bg-black z-10"
-            style={{
-              transition: ".5s",
-            }}
           ></span>
         </div>
       </div>
