db0d63e0803201e879ce62f67806acc0b903974c
Darksied95 <aliyuefe9501@gmail.com>
2023-04-13 15:11:54 +0100

    extracted data

--- src/Component/Main/SectionFour/SingleArticle.jsx
+++ src/Component/Main/SectionFour/SingleArticle.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import img from "../../../Assets/silhouette.webp";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import style from "./style.module.css";
-const SingleArticle = () => {
+const SingleArticle = ({ img, heading, press }) => {
   return (
-    <div className={`${style.grid} border-t-2 border-black pt-4 font-bold`}>
+    <div
+      className={`${style.grid} border-t-2 border-black pt-4 font-bold mb-10`}
+    >
       <img src={img} alt="" className={`${style["grid_img"]}`} />
       <h6
         className={`${style.grid_heading} text-2xl font-semibold opacity-70 mt-5 mb-10 md:mt-0 md:ml-4 xl:text-5xl`}
       >
-        Introducing Rev by Lyft
+        {heading}
       </h6>
       <p
         className={`${style.grid_press} text-xs  md:flex items-end md:ml-4 md:text-base`}
       >
-        Press <span className="font-normal">3.28.23</span>
+        Press <span className="font-normal">{press}</span>
       </p>
       <div className={`${style["grid_arrow"]}`}>
         <AiOutlineArrowRight className="w-full  h-full" />
---
+++ src/Component/Main/SectionFour/data.js
@@ -0,0 +1,44 @@
+import Silouhette from "../../../Assets/silhouette.webp"
+import Flower from "../../../Assets/Flower.webp";
+import NewsPaper from "../../../Assets/Good Design.webp";
+import WeirdThing from "../../../Assets/weirdsomething.webp";
+import Gold from "../../../Assets/goldPic.webp";
+import Jams from "../../../Assets/jamsEarn.webp";
+
+
+
+
+
+
+export const data = [
+    {
+        img: Silouhette,
+        press: "3.28.23",
+        heading: "Introducing REV by LYFT"
+    },
+    {
+        img: Flower,
+        press: "4.11.33",
+        heading: "BASIC/DEPT® is Ad Age's A-List Design & Branding Agency of the Year"
+    },
+    {
+        img: NewsPaper,
+        press: "67.1.45",
+        heading: "The Principles of Good Design: Good Design is Glamorous"
+    },
+    {
+        img: WeirdThing,
+        press: "99.99.01",
+        heading: "5 ways to fall in love with learning about accessibility "
+    },
+    {
+        img: Gold,
+        press: "1.1.1.1",
+        heading: "How brands can represent people with disabilities with authenticity"
+    },
+    {
+        img: Jams,
+        press: "1.1.1.1",
+        heading: "How apparel brands can embrace gender fluidity in the metaverse"
+    },
+]
