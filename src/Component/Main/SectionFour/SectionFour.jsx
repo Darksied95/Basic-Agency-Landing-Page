83edfda34426fd082ee9645c2c8e8f5be5f32d38
Darksied95 <aliyuefe9501@gmail.com>
2023-04-14 17:07:40 +0100

    added section4 finishing style

--- src/Component/Main/SectionFour/SectionFour.jsx
+++ src/Component/Main/SectionFour/SectionFour.jsx
@@ -4,8 +4,8 @@ import { data } from "./data";
 
 const SectionFour = () => {
   return (
-    <section className="uppercase">
-      <div className="flex justify-between items-center mb-11 ">
+    <section className="uppercase max-w-[90vw] mx-auto">
+      <div className="flex justify-between items-center mb-11">
         <h6 className="font-bold text-xl xl:text-[2.6rem] xl:leading-tight">
           Featured <br className="md:hidden xl:block" /> News
         </h6>
--- src/Component/Main/SectionFour/SingleArticle.jsx
+++ src/Component/Main/SectionFour/SingleArticle.jsx
@@ -3,9 +3,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import style from "./style.module.css";
 const SingleArticle = ({ img, heading, press }) => {
   return (
-    <div
-      className={`${style.grid} border-t-2 border-black pt-4 font-bold mb-10`}
-    >
+    <div className={`${style.grid} border-t border-black pt-4 font-bold mb-10`}>
       <img src={img} alt="" className={`${style["grid_img"]}`} />
       <h6
         className={`${style.grid_heading} text-2xl font-semibold opacity-70 mt-5 mb-10 md:mt-0 md:ml-4 xl:text-5xl`}
@@ -15,7 +13,8 @@ const SingleArticle = ({ img, heading, press }) => {
       <p
         className={`${style.grid_press} text-xs  md:flex items-end md:ml-4 md:text-base`}
       >
-        Press <span className="font-normal">{press}</span>
+        Press
+        <span className="font-normal">{press}</span>
       </p>
       <div className={`${style["grid_arrow"]}`}>
         <AiOutlineArrowRight className="w-full  h-full" />
