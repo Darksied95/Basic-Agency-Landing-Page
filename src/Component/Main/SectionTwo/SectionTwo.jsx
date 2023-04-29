979716232a24063030df49aaa08620f7246fe560
Darksied95 <aliyuefe9501@gmail.com>
2023-04-29 18:28:17 +0100

    added section 2  hover switch

--- src/Component/Main/SectionTwo/SectionTwo.jsx
+++ src/Component/Main/SectionTwo/SectionTwo.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ArticleCarousel from "./ArticleCarousel";
 import ArticleSlider from "./ArticleSlider";
 import SectionTwoHover from "./SectionTwoHover";
@@ -9,6 +9,12 @@ const SectionTwo = () => {
   const sectionRef = useRef();
   const { showCursor, handleShowCursor, handleHideCursor } = useHover();
 
+  const [clicked, setClicked] = useState(false);
+
+  const handleMouseUp = () => setClicked(false);
+
+  const handleMouseDown = () => setClicked(true);
+
   function updateSlider(num) {
     let stringinfyNum = num + "%";
     sliderCounter.current.style.left = stringinfyNum;
@@ -16,7 +22,12 @@ const SectionTwo = () => {
   }
 
   return (
-    <section className="xl:mb-36 relative" ref={sectionRef}>
+    <section
+      className="xl:mb-36 relative"
+      ref={sectionRef}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
       <h1 className="text-2xl leading-6 font-bold ml-[5vw] uppercase xl:ml-20 xl:text-5xl xl:mt-28">
         Featured
         <br className="md:hidden xl:block" />
@@ -27,6 +38,7 @@ const SectionTwo = () => {
         customRef={sectionRef}
         showCursor={handleShowCursor}
         hideCursor={handleHideCursor}
+        clicked={clicked}
       />
 
       <ArticleCarousel updateSlider={updateSlider} />
--- src/Component/Main/SectionTwo/SectionTwoHover.jsx
+++ src/Component/Main/SectionTwo/SectionTwoHover.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Hover from "./../../Hover/Hover";
 
-const SectionTwoHover = ({ customRef, showCursor, hideCursor }) => {
+const SectionTwoHover = ({ customRef, showCursor, hideCursor, clicked }) => {
   return (
     <Hover
       customRef={customRef}
       showCursor={showCursor}
       hideCursor={hideCursor}
     >
-      <div className="bg-[#f9cdcdff] rounded-full w-full aspect-square font-bold text-xs grid place-content-center">
-        DRAG
-      </div>
+      {!clicked ? (
+        <div className="bg-[#f9cdcdff] rounded-full w-full aspect-square font-bold text-xs grid place-content-center">
+          DRAG
+        </div>
+      ) : (
+        <div className="relative ">
+          <div className="bg-[#f9cdcdff] rounded-full w-[80%] aspect-square font-bold text-xs grid place-content-center m-auto"></div>
+          <span className="border-[10px] border-transparent border-r-[#f9cdcdff] absolute top-1/2 -left-6"></span>
+          <span className="border-[10px] border-transparent border-l-[#f9cdcdff] absolute top-1/2 -right-6"></span>
+        </div>
+      )}
     </Hover>
   );
 };
